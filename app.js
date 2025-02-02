901e74c77ac00d7aed6c4659e2d065107ef75df7
BeehnoodLab <behnood.pazhang@yahoo.com>
2025-02-02 11:16:56 +0100

    Redirect en express

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ const express = require("express")
 // express app
 const app = express()
 
-
-app.get('/', function (req, res) {
-  res.sendFile('./view/index.html', {root : __dirname})
-})
-
-app.get('/about', function (req, res) {
-    res.sendFile('./view/about.html', {root : __dirname})
-  })
-  
-
-
 //  listen for req
 app.listen(3000)
+
+app.get('/', function (require, response) {
+  response.sendFile('./view/index.html', {root : __dirname})
+})
+
+app.get('/about', function (require, response) {
+    response.sendFile('./view/about.html', {root : __dirname})
+  })
+  
+// redirecting
+app.get('/about-us', (require, response) => {
+    response.redirect('/about')
+})
+
+// 404 page
+app.use((require, response)=>{
+response.sendFile('./view/404.html', {root : __dirname})
+})
