fea5abf3c9330e07943b0970c1726d88e25ff96a
BeehnoodLab <behnood.pazhang@yahoo.com>
2025-02-02 11:03:53 +0100

    Routing et html pages en express

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const app = express()
 
 
 app.get('/', function (req, res) {
-  res.send('World Says Hello To Behnood')
+  res.sendFile('./view/index.html', {root : __dirname})
 })
+
+app.get('/about', function (req, res) {
+    res.sendFile('./view/about.html', {root : __dirname})
+  })
+  
+
 
 //  listen for req
 app.listen(3000)
