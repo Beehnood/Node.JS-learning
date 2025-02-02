9ed24755da64deaaf3ac3415cc4959931be47faf
BeehnoodLab <behnood.pazhang@yahoo.com>
2025-02-02 12:29:11 +0100

    Création des views engins.ejs et index.ejs pour le rendu Express

--- app.js
+++ app.js
@@ -3,23 +3,27 @@ const express = require("express")
 // express app
 const app = express()
 
+// reggister view engine
+app.set('view engine','ejs')
+
 //  listen for req
 app.listen(3000)
 
 app.get('/', function (require, response) {
-  response.sendFile('./view/index.html', {root : __dirname})
+  response.render('index')
 })
 
 app.get('/about', function (require, response) {
-    response.sendFile('./view/about.html', {root : __dirname})
-  })
+    response.render('about')  })
+
+app.get('/blogs/create', function (require, response) {
+    response.render('create')  })
   
 // redirecting
-app.get('/about-us', (require, response) => {
-    response.redirect('/about')
-})
+// app.get('/about-us', (require, response) => {
+//     response.redirect('/about')
+// })
 
 // 404 page
 app.use((require, response)=>{
-response.sendFile('./view/404.html', {root : __dirname})
-})
+    response.status(404).render('404')})
