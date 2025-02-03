e277e23e06d2a5b8c544b70612b74e493ca85291
BeehnoodLab <behnood.pazhang@yahoo.com>
2025-02-03 14:17:33 +0100

    Intégration des données dynamiques dans les vues EJS avec Express

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.use(morgan("morgan"));
 // app.get("/add-blog", (req, res) => {  
     
 //     const blog = new Blog({
-//         title: "new blog 3",
-//         snippet: "about my new blog 3",
+//         title: "new blog 4",
+//         snippet: "about my new blog 4",
 //         body:"more about my new blog",
 //     })
 //     blog.save()
@@ -28,40 +28,40 @@ app.use(morgan("morgan"));
 
 
 // trouver tous les blogs
-app.get("/all-blog", (req, res) => {    
-    Blog.find()
-        .then((result) => res.send(result))
-        .catch((err) => console.log(err));
+// app.get("/all-blog", (req, res) => {    
+//     Blog.find()
+//         .then((result) => res.send(result))
+//         .catch((err) => console.log(err));
 
-});
+// });
 
 // trouver un blog
-app.get("/single-blog", (req, res) => {    
-    Blog.findById('67a070a48d59b49c7086e2f0')
-        .then((result)=> res.send(result)
-        .catch((err)=> console.log(err)))
+// app.get("/single-blog", (req, res) => {    
+//     Blog.findById('67a070a48d59b49c7086e2f0')
+//         .then((result)=> res.send(result)
+//         .catch((err)=> console.log(err)))
 
-});
+// });
 
 // supprimer un blog
-app.get("/rm-single-blog", (req, res)=>{
-    Blog.findByIdAndDelete('67a0a0968fc4e657c9626e1a')
-    .then((result)=> {
-        if(result){
-            res.send(`blog supprimé: ${result}`)
-    }else{
-        res.send('blog non trouvé')
-    }
-})
-    .catch((err)=> console.log(err))
-})
+// app.get("/rm-single-blog", (req, res)=>{
+//     Blog.findByIdAndDelete('67a0a0968fc4e657c9626e1a')
+//     .then((result)=> {
+//         if(result){
+//             res.send(`blog supprimé: ${result}`)
+//     }else{
+//         res.send('blog non trouvé')
+//     }
+// })
+//     .catch((err)=> console.log(err))
+// })
 
 // supprimer tous les blogs
-app.get("/rm-all-blogs", (req, res)=>{
-    Blog.deleteMany({})
-     .then((result)=> res.send(result))
-     .catch((err)=> console.log(err))
-})
+// app.get("/rm-all-blogs", (req, res)=>{
+//     Blog.deleteMany({})
+//      .then((result)=> res.send(result))
+//      .catch((err)=> console.log(err))
+// })
 
 
 // Connexion à la base de données (MongoDB)
@@ -75,14 +75,19 @@ mongoose
   })
   .catch((err) => console.error(" Erreur de connexion MongoDB :", err));
 
-// Page d'accueil avec blogs
+
+// Routes
+// Page d'accueil avec blogs à la main
 app.get("/", (req, res) => {
-  const blogs = [
-    { title: "Nader trouve la NASA", snippet: "Lorem ipsum dolor sit amet..." },
-    { title: "Ghassem découvre Git", snippet: "Lorem ipsum dolor sit amet..." },
-    { title: "Jerard trouve LeBonCoin", snippet: "Lorem ipsum dolor sit amet..." },
-  ];
-  res.render("index", { title: "Home", blogs });
+    // Page d'accueil avec blogs à la main
+//   const blogs = [
+//     { title: "Nader trouve la NASA", snippet: "Lorem ipsum dolor sit amet..." },
+//     { title: "Ghassem découvre Git", snippet: "Lorem ipsum dolor sit amet..." },
+//     { title: "Jerard trouve LeBonCoin", snippet: "Lorem ipsum dolor sit amet..." },
+//   ];
+//   res.render("index", { title: "Home", blogs });
+    res.redirect("/blogs");
+
 });
 
 // Page About
@@ -90,15 +95,24 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
+// routes des blogs
+app.get("/blogs", (req, res) => {
+    Blog.find().sort({ createdAt:-1 })
+      .then((blogs) => {
+        res.render("index", { title: "All Blogs", blogs });
+      })
+      .catch((err) => console.log(" Erreur lors de la récupération des blogs :", err));
+  });
+
 // Page de création d'un blog
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create" });
 });
 
 // Redirection
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
-});
+// app.get("/about-us", (req, res) => {
+//   res.redirect("/about");
+// });
 
 // Page 404 (toujours à la fin)
 app.use((req, res) => {
